b0a9f8886acf95eefced9eda4b7641d083221fa3
Gabriel <gabrielgvo16@gmail.com>
2026-04-20 09:33:37 -0300

    build: configure the models

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Follow extends Model
+class Follow extends Pivot
 {
-    //
+    protected $table = 'follows';
+
+    public function follower()
+    {
+        return $this->belongsTo(User::class, 'follower_id');
+    }
+
+    public function followedUser()
+    {
+        return $this->belongsTo(User::class, 'following_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,26 +2,51 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'avatar',
+        'username',
+        'bio',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function likedPosts()
+    {
+        return $this->belongsToMany(Post::class, 'likes')->withTimestamps();
+
+    }
+
+    public function following() {
+        return $this->belongsToMany(User::class, 'follows', 'follower_id', 'following_id')
+            ->using(Follow::class)
+            ->withTimestamps();
+    }
+
+        public function followers() {
+        return $this->belongsToMany(User::class, 'follows', 'following_id', 'follower_id')
+            ->using(Follow::class)
+            ->withTimestamps();
+    }
+
     protected function casts(): array
     {
         return [
